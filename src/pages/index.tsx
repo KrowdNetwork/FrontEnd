bb00f08bb4b715b8160e569ce709bde2be553b93
Itallo <dornelaszxd@gmail.com>
2022-12-06 18:41:29 -0300

    feat(pages): remove redirect to home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,13 @@
 import { Grid } from "@chakra-ui/react";
 import { useSession } from "next-auth/react";
 
-import HomePage from "lib/pages/home";
-
 import Signin from "./auth/signin";
+import AuthorizedSocialMedia from "./authorizedSocialMedia";
 
 const Home = () => {
   const { data: session } = useSession();
 
-  return <Grid>{session ? <HomePage /> : <Signin />}</Grid>;
+  return <Grid>{session ? <AuthorizedSocialMedia /> : <Signin />}</Grid>;
 };
 
 export default Home;
